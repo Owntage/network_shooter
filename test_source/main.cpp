f987dcc5272a6fe3ac25d90792796ba5eea0f32d
= <igor.ilyin@jetbrains.com>
2017-06-06 10:03:39 -0700

    gui manager added

--- test_source/main.cpp
+++ test_source/main.cpp
@@ -6,6 +6,10 @@ using namespace std;
 
 int main()
 {
+	GuiManager guiManager(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+
+
 	while(RenderWindow::getInstance()->window.isOpen())
 	{
 		RenderWindow::getInstance()->window.clear();
@@ -13,9 +17,22 @@ int main()
 		sf::Event event;
 		while(RenderWindow::getInstance()->window.pollEvent(event))
 		{
-			if(event.type == sf::Event::Closed)
+			switch (event.type)
 			{
+			case sf::Event::MouseButtonPressed:
+				guiManager.onMouseClick(event.mouseButton.x - WINDOW_WIDTH / 2, event.mouseButton.y - WINDOW_HEIGHT / 2, false);
+				break;
+			case sf::Event::MouseButtonReleased:
+				guiManager.onMouseClick(event.mouseButton.x - WINDOW_WIDTH / 2, event.mouseButton.y - WINDOW_HEIGHT / 2, true);
+				break;
+			case sf::Event::MouseMoved:
+				guiManager.onMouseMove(event.mouseMove.x - WINDOW_WIDTH / 2, event.mouseMove.y - WINDOW_HEIGHT / 2);
+				break;
+			case sf::Event::Closed:
 				RenderWindow::getInstance()->window.close();
+				break;
+			default:
+				break;
 			}
 		}
 	}
